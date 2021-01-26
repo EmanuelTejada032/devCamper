f2cb89d19f4fe1e80d2ff0f3c32fa4f29bef57eb
Emanuel <emanueltejada31295@gmail.com>
2021-01-26 18:35:18 -0400

    connect to mongo db and add bootcamp model

--- server.js
+++ server.js
@@ -2,25 +2,41 @@ const express = require('express');
 const  dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan')
+const connectDB = require('./config/db');
+
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+const app = express();
+
+
+//connect to db
+connectDB();
 
 
 //Routes files
  const bootcamps = require('./routes/campgrounds');
+ 
 
-//load env vars
-
-dotenv.config({ path: './config/config.env' });
-const app = express();
-
-// app.use(logger);
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
+ 
+ // app.use(logger);
+ if(process.env.NODE_ENV === 'development'){
+     app.use(morgan('dev'));
+ }
+ 
 app.use('/api/v1/bootcamps', bootcamps);
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`)
 });
+
+//Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+
+    //close the server
+    server.close(() => process.exit(1))
+})
